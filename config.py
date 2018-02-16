0d08d21773c3f1bb3f65fb0a989c1b1b4f72d49c
Greg Lackey <gdl5014@gmail.com>
2018-02-16 14:37:11 -0700

    added scrape_doc directory, example docs, and path in config

--- config.py
+++ config.py
@@ -16,4 +16,4 @@ class Config(object):
             'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ADMINS= ['gdl5014@gmail.com']
-
+    UPLOAD_FOLDER = os.path.join(basedir,'scrape_docs')
