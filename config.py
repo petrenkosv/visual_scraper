4c7ea3261a916a0f048d8b3b067a00195daa1496
Greg Lackey <gdl5014@gmail.com>
2018-02-16 13:57:39 -0700

    committing base app

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    POSTGRES = {
+        'user':'greg',
+        'pw':'lackey',
+        'db':'visual_scraper',
+        'host':'localhost',
+        'port':'5432',
+    }
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ADMINS= ['gdl5014@gmail.com']
+
